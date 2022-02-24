db4526fa42e10062be206fccbc25a250a230b2d5
wong shin guey <shinguey.wong@sophicautomation.com>
2022-02-24 18:15:00 +0800

    add responsive for header

--- src/Main.jsx
+++ src/Main.jsx
@@ -23,28 +23,28 @@ function HeaderNavigation() {
           <a href="#" class="text-2xl font-bold text-teal-800">
             Beginner Tailwind
           </a>
-          <a href="#" class="hover:text-gray-900">
+          <a href="#" class="hidden hover:text-gray-900 lg:inline-block">
             About
           </a>
-          <a href="#" class="hover:text-gray-900">
+          <a href="#" class="hidden hover:text-gray-900 lg:inline-block">
             Waht You'll Learn
           </a>
-          <a href="#" class="hover:text-gray-900">
+          <a href="#" class="hidden hover:text-gray-900 lg:inline-block">
             Pricing
           </a>
-          <a href="#" class="hover:text-gray-900">
+          <a href="#" class="hidden hover:text-gray-900 lg:inline-block">
             FAQ
           </a>
         </div>
 
         {/* right header section */}
-        <div class="space-x-4">
+        <div class="flex items-center space-x-4">
           <a href="#" class="hover:text-gray-900">
             Login
           </a>
           <a
             href="#"
-            class="inline-block rounded bg-purple-200 p-3 text-purple-600 shadow transition duration-500 hover:bg-purple-400 hover:text-purple-300 hover:shadow-xl"
+            class="hidden rounded bg-purple-200 p-3 text-purple-600 shadow transition duration-500 hover:bg-purple-400 hover:text-purple-300 hover:shadow-xl lg:inline-block"
           >
             Get the Course
           </a>
