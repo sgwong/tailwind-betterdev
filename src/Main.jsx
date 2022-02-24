27c64ccf5f7e7fd1f266b5fb383f0fc0c66aa108
wong shin guey <shinguey.wong@sophicautomation.com>
2022-02-24 16:40:27 +0800

    initial commit

---
+++ src/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div class="flex min-h-screen items-center justify-center bg-teal-400 text-6xl text-teal-100">
+      <h2>hello i am a website!</h2>
+    </div>
+  );
+}
+
+export default Main;
